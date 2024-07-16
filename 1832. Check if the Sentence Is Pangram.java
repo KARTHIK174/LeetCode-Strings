f8eaf56e28e196878cf28c284991ab69aa6c88e1
KOLLI SRI KRISHNA KARTHIKEYA <107982190+KARTHIK174@users.noreply.github.com>
2024-07-16 19:04:41 +0530

    1832. Check if the Sentence Is Pangram.java

---
+++ 1832. Check if the Sentence Is Pangram.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        if(sentence.length()<26) return false;
+        HashSet<Character> set = new HashSet<>();
+        for(char ch:sentence.toCharArray()){
+            set.add(ch);
+        }
+        return set.size()==26;
+    }
+}
